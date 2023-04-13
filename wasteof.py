c7f50d79bd5fa235b91afdcdc08e4a09e04f4634
radeeyate <124477460+radeeyate@users.noreply.github.com>
2023-04-13 07:37:41 -0600

    add auth stuff

--- wasteof.py
+++ wasteof.py
@@ -1,5 +1,8 @@
 import requests
 
+class AuthenticationError(Exception):
+    pass
+
 def TopUsers():
     r = requests.get('https://api.wasteof.money/explore/users/top')
     return(r.json())
@@ -13,5 +16,14 @@ def userToId(id):
     r = requests.get("https://api.wasteof.money/username-from-id/" + id)
     j = r.json()
     return(j["username"])
+
+def getSession(username, password):
+    r = requests.post("https://api.wasteof.money/session", json = {"username": username, "password": password})
+    j = r.json()
+    try:
+        return(j["token"])
+    except:
+        if "error" in j:
+            raise AuthenticationError(j["error"])
 
 
