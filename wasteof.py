5f30b8f5adfd175017501ff19a24fa17c53786db
radeeyate <124477460+radeeyate@users.noreply.github.com>
2023-04-13 09:57:36 -0600

    added functions to classes + switched to f-strings

--- wasteof.py
+++ wasteof.py
@@ -3,27 +3,27 @@ import requests
 class AuthenticationError(Exception):
     pass
 
-def TopUsers():
-    r = requests.get('https://api.wasteof.money/explore/users/top')
-    return(r.json())
+class users():
+    def topUsers():
+        r = requests.get('https://api.wasteof.money/explore/users/top')
+        return(r.json())
 
-def trending(timeframe = "week"):
-    print(timeframe)
-    r = requests.get('https://api.wasteof.money/explore/posts/trending?timeframe=' + timeframe)
-    return(r.json())
+    def trending(timeframe = "week"):
+        r = requests.get(f'https://api.wasteof.money/explore/posts/trending?timeframe={timeframe}')
+        return(r.json())
 
-def userToId(id):
-    r = requests.get("https://api.wasteof.money/username-from-id/" + id)
-    j = r.json()
-    return(j["username"])
-
-def getSession(username, password):
-    r = requests.post("https://api.wasteof.money/session", json = {"username": username, "password": password})
-    j = r.json()
-    try:
-        return(j["token"])
-    except:
-        if "error" in j:
-            raise AuthenticationError(j["error"])
+    def IdToUser(id):
+        r = requests.get(f"https://api.wasteof.money/username-from-id/{id}")
+        j = r.json()
+        return(j["username"])
+class session():
+    def get(username, password):
+        r = requests.post("https://api.wasteof.money/session", json = {"username": username, "password": password})
+        j = r.json()
+        try:
+            return(j["token"])
+        except:
+            if "error" in j:
+                raise AuthenticationError(j["error"])
 
 
